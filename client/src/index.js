e68a9dbd9a50f4a6e4d8cd8ec39af1a6d22ee92b
Pratyek Thumula <pratyekpk3@gmail.com>
2025-04-25 07:01:58 +0530

    fixed

--- client/src/index.js
+++ client/src/index.js
@@ -3,36 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import axios from 'axios';
-
-// Set default axios base URL
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3002';
-
-// Add axios interceptor to automatically add auth token to requests
-axios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['x-auth-token'] = token;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-// Add response interceptor to handle 401 errors
-axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
 
 ReactDOM.render(
   <React.StrictMode>
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 // client/src/services/api.js
 import axios from 'axios';
+import { API_URL } from '../config';
 
 // Base URL for API endpoints
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3002';
 
 // Create axios instance with base URL
 const api = axios.create({
